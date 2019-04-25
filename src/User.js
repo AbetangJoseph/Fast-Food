d8b45210790d23150f8d59efdc10831e3d9d4431
Abetang Joseph <josephabetang@gmail.com>
2019-04-25 17:15:35 +0100

    Add saveAccount method to User prototype

---
+++ src/User.js
@@ -0,0 +1,38 @@
+const DB = require("./db");
+
+// User ID
+let userId = 0;
+
+// User Constructor
+function User(name, password, email, phone, address) {
+   userId++;
+   this.userId = userId;
+   this.name = name;
+   this.password = password;
+   this.email = email;
+   this.phone = phone;
+   this.address = address;
+   this.isDeleted = false;
+}
+
+// Adding Methos to User prototype
+User.prototype = {
+   constructor: User,
+
+   saveAccount: function() {
+      user_payload = {
+         id: this.userId,
+         name: this.name,
+         password: this.password,
+         email: this.email,
+         phone: this.phone,
+         address: this.address,
+         isDeleted: this.isDeleted
+      };
+
+      DB["USERS"].push(user_payload);
+      return "SUCCESS: Account Saved";
+   }
+};
+
+module.exports = User;
