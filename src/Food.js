a6cdba2ff64a7a359bd41c37e40881f02a379126
Abetang Joseph <josephabetang@gmail.com>
2019-04-26 13:10:39 +0100

    Undo mixins and use normal prototype style And Add deleteAllFoodItem method to prototype

--- src/Food.js
+++ src/Food.js
@@ -3,17 +3,17 @@ const DB = require("./db");
 // Food ID
 let foodId = 0;
 
-function Food(name, category, price, quantity) {
+function Food(foodName, category, price, quantity) {
    foodId++;
    this.foodId = foodId;
-   this.name = name;
+   this.foodName = foodName;
    this.category = category;
    this.price = price;
    this.quantity = quantity;
 
    food_payload = {
       id: this.foodId,
-      name: this.name,
+      foodName: this.foodName,
       category: this.category,
       price: this.price,
       quantity: this.quantity,
@@ -25,39 +25,40 @@ function Food(name, category, price, quantity) {
 }
 
 // Adding Methods to Food prototype
-Food.prototype = {
-   constructor: Food,
+Food.prototype.createFoodItem = function(foodName, category, price, quantity) {
+   return new Food(foodName, category, price, quantity);
+};
 
-   createFoodItem: function(name, category, price, quantity) {
-      return new Food(name, category, price, quantity);
-   },
+Food.prototype.updateFoodItem = function(
+   foodId,
+   foodName,
+   category,
+   price,
+   quantity
+) {
+   DB.FOOD.foodStore.find(food => {
+      if (food.id === foodId) {
+         food.foodName = foodName || food.foodName;
+         food.category = category || food.category;
+         food.price = price || food.price;
+         food.quantity = quantity || food.quantity;
+      }
+   });
+   return "SUCCESS: Food Updated";
+};
 
-   updateFoodItem: function(
-      foodToUpdate,
-      newFoodName,
-      category,
-      price,
-      quantity
-   ) {
-      DB.FOOD.foodStore.find(food => {
-         if (food.name === foodToUpdate) {
-            food.name = newFoodName || food.name;
-            food.category = category || food.category;
-            food.price = price || food.price;
-            food.quantity = quantity || food.quantity;
-         }
-      });
-      return "SUCCESS: Food Updated";
-   },
+Food.prototype.deleteOneFoodItem = function(foodId) {
+   DB.FOOD.foodStore.find(food => {
+      if (food.id === foodId) {
+         food.isDeleted = true;
+      }
+   });
+   return "SUCCESS: Food Item Deleted";
+};
 
-   deleteOneFoodItem: function(foodId) {
-      DB.FOOD.foodStore.find(food => {
-         if (food.id === foodId) {
-            food.isDeleted = true;
-         }
-      });
-      return "SUCCESS: Food Item Deleted";
-   }
+Food.prototype.deleteAllFoodItem = function() {
+   DB.FOOD.foodStore.map(food => (food.isDeleted = true));
+   return "SUCCESS: All Food Items Deleted";
 };
 
 module.exports = Food;
