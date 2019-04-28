767611d2d064084e1a12e9ab0250a7f1a127b020
Abetang Joseph <josephabetang@gmail.com>
2019-04-28 01:33:18 +0100

    Test: Admin can create food item

--- tests/unit.test.js
+++ tests/unit.test.js
@@ -1,27 +1,40 @@
 const User = require("../src/User");
+const Admin = require("../src/Admin");
+const Food = require("../src/Food");
+const db = require("../src/db");
 
-describe("CREATE NEW USER", function() {
-   it("Should return a value when called", function() {
-      const joe = new User(
-         "Joseph",
-         "pass1",
-         "joe@gmail",
-         "08312929393",
-         "Lekki"
-      );
+const admin1 = new Admin("Joseph", "pass1", "admin1@gmail", "08312929393");
+const admin2 = new Admin("Paul", "pass2", "admin2@gmail", "08312944493");
+const user1 = new User("John", "pass1", "joe@gmail", "08312929393", "Lekki");
+const user2 = new User("Jane", "pass2", "jane@gmail", "08310009393", "Ajao");
+const user3 = new User("Victor3", "pass3", "vic@gmail", "08310009393", "Ikeja");
+admin2.saveAccount();
+user3.saveAccount();
 
-      expect(joe.saveAccount()).toBeDefined();
+user2.saveAccount();
+
+describe("CREATE NEW ADMIN USER", function() {
+   const savedAdmin1 = admin1.saveAccount();
+
+   it("Should return success if admin account saved to db", function() {
+      expect(savedAdmin1).toMatch("SUCCESS: Account Saved");
    });
+});
 
-   it("Should create and save users account to DB", function() {
-      const joe = new User(
-         "Joseph",
-         "pass1",
-         "joe@gmail",
-         "08312929393",
-         "Lekki"
+describe("ADMIN CAN CREATE FOOD ITEM", function() {
+   it("Should return success if food was created", function() {
+      expect(admin1.createFoodItem("rice", "grains", 500, 18)).toMatch(
+         "SUCCESS: Food Added"
       );
 
-      expect(joe.saveAccount()).toMatch("SUCCESS: Account Saved");
+      expect(admin1.createFoodItem("beans", "protein", 450, 28)).toMatch(
+         "SUCCESS: Food Added"
+      );
+      expect(admin1.createFoodItem("yoghurt", "dairy", 600, 8)).toMatch(
+         "SUCCESS: Food Added"
+      );
+      expect(admin1.createFoodItem("Orange", "fruit", 100, 20)).toMatch(
+         "SUCCESS: Food Added"
+      );
    });
 });
